94015bb06fb6090f3f34e81d6bab497414081a8d
Rajesh Reddy <rajeshreddy2427@gmail.com>
2019-11-04 08:12:17 +0530

    first commit

---
+++ src/Handling_Drop_downs/Drop_downs.java
@@ -0,0 +1,47 @@
+package Handling_Drop_downs;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Drop_downs {
+
+	public static void main(String[] args) {
+	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rajesh Reddy\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.spicejet.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	    driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXTaction")).click();
+	    //driver.findElement(By.xpath("//a[@value='GOI']")).click();
+	    //This is parent child relationship
+	    driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='GOI']")).click();
+	    driver.findElement(By.id("ctl00_mainContent_ddl_destinationStation1_CTXTaction")).click();
+	    driver.findElement(By.xpath("(//a[@value='HYD'])[2]")).click();
+	   
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/Handling_Drop_downs/Select_Dropdown.java
@@ -0,0 +1,43 @@
+package Handling_Drop_downs;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Select_Dropdown {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rajesh Reddy\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.cleartrip.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		//driver.findElement(By.xpath("//input[@id='From']")).click();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
